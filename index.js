ca60b2fa1ce6458e2104e2f3e033d8bb47e92841
Zohaib <zohaibmanzoor1700@gmail.com>
2021-11-10 15:42:45 +0000

    completed

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-//write your code here
+const stripUpperCase = str => {
+// Initialize empty strings to hold lowercased values
+  let lowerCased = "";
+// Loop through the str
+  for (const i of str) {
+// Check if any character is lowercased
+    if(i === i.toLowerCase()) {
+// Push the values that evaluate to true into variable
+      lowerCased += i;
+    }
+// Return the variable 
+  }return lowerCased;
+};
+console.log(stripUpperCase("ilOvEcOdInG...!"));
